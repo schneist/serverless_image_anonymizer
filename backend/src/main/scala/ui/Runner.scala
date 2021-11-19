214134c17bcb80ea9d4a2402d698a9d3ccc36cba
stefan.schneider <stefan.schneider@tum.de>
2021-11-19 11:56:19 +0100

    first load suiccess

--- backend/src/main/scala/ui/Runner.scala
+++ backend/src/main/scala/ui/Runner.scala
@@ -21,16 +21,21 @@ object Runner  {
 
   def main(args: Array[String]): Unit = {
     val fetch : (String, scala.scalajs.js.UndefOr[typings.std.RequestInit]) => scala.scalajs.js.Promise[typings.std.Response] = (s:String,r:scala.scalajs.js.UndefOr[typings.std.RequestInit] ) ⇒ {
-      r.map(rr ⇒ typings.crossFetch.mod.fetch(s,rr)).getOrElse({println("Blubb");reject(Promise)})
+      val ff  = r.map(rr ⇒ typings.crossFetch.mod.fetch(s,rr))
+      ff.getOrElse({
+        println("Fetch returned undef")
+        reject(Promise)
+      })
     }
+    val createcanvas :() => typings.std.HTMLCanvasElement = ??? //typings.canvas.mod.createCanvas()
     val pe = PartialEnvironment()
     pe.setFetch(fetch)
+    pe.setCreateCanvasElement(createcanvas)
     env.monkeyPatch(env = pe )
     val aa = for {
-      a <- loadSsdMobilenetv1Model(MODEL_URL).toFuture
-      b <- loadFaceLandmarkModel(MODEL_URL).toFuture
-      c <- loadFaceRecognitionModel(MODEL_URL).toFuture
-    } yield a
+      a <- nets.ssdMobilenetv1.loadFromDisk(MODEL_URL).toFuture
+      c <- nets.faceRecognitionNet.loadFromDisk(MODEL_URL).toFuture
+    } yield (a,c)
 
     js.timers.setTimeout(500) {
       println(aa.value)
