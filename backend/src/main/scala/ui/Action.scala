83ff58533e9309ba32b26e91c2945493ceb76087
stefan.schneider <stefan.schneider@tum.de>
2021-11-26 09:09:56 +0100

    refactor into actions

--- backend/src/main/scala/ui/Action.scala
+++ backend/src/main/scala/ui/Action.scala
@@ -3,8 +3,10 @@ package ui
 import cats.MonadError
 import cats.data.EitherT
 import cats.effect.IO
+import typings.node.bufferMod.global.Buffer
 import typings.tensorflowTfjsCore.distTensorMod.{Tensor3D, Tensor4D}
 
+import java.io.FileNotFoundException
 import scala.concurrent.{ExecutionContext, Future}
 import scala.scalajs.js.typedarray.Uint8Array
 import scala.util.{Failure, Success, Try}
@@ -23,15 +25,20 @@ object LoadImageAction extends Action[EnvironmentWithExecutionContext,String,Uin
              (using environment: EnvironmentWithExecutionContext,
               me: cats.MonadError[cats.effect.IO, LoadImageErrors]
              ):cats.data.EitherT[cats.effect.IO, LoadImageErrors, Uint8Array] = {
-    ???
-    /**
-    * implicit val ec = environment.getExecutionContext()
+    implicit val ec = environment.getExecutionContext()
     for {
-      checkFilePath ← checkFileAccess(filepath)
-      image ← EitherT.left(IO.fromFuture(IO(typings.node.fsPromisesMod.readFile(filepath).toFuture)))
-      uint8 ← EitherT.left(IO(image.asInstanceOf[Uint8Array]))
-    } yield uint8
-    */
+      image ← EitherT(
+        IO.fromFuture(IO(
+          typings.node.fsPromisesMod.readFile(filepath).toFuture.map(a ⇒ {println("################################");a.asInstanceOf[Uint8Array]}).transform { a ⇒
+            println("################################")
+            a match {
+              case f: Failure[Throwable] ⇒ Try(Left(FileNotFound()))
+              case s: Success[Buffer] ⇒ Try(Right(s))
+            }
+          }
+        ))
+      )
+    } yield image.get
   }
 
   def checkFileAccess(filepath: String)
@@ -39,7 +46,6 @@ object LoadImageAction extends Action[EnvironmentWithExecutionContext,String,Uin
                       me: cats.MonadError[cats.effect.IO, LoadImageErrors]
                      ):cats.data.EitherT[cats.effect.IO, LoadImageErrors, Unit] = {
     implicit val ec = environment.getExecutionContext()
-    println(ec)
     EitherT {
       IO.fromFuture(IO(
         typings.node.fsPromisesMod.access(filepath).toFuture.transform {
--- backend/src/test/scala/action/ActionTest.scala
+++ backend/src/test/scala/action/ActionTest.scala
@@ -19,6 +19,24 @@ import scala.language.postfixOps
 import scala.Unit
 import ui.FileNotFound
 
+
+class MySpec extends AsyncFreeSpec with AsyncIOSpec with Matchers {
+  val existingPath = "/images/stefan.png"
+  val me = summon[cats.MonadError[cats.effect.IO, LoadImageErrors]]
+
+  given EnvironmentWithExecutionContext = new EnvironmentWithExecutionContext {
+    override def getExecutionContext(): ExecutionContext = scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
+  }
+
+  given cats.MonadError[cats.effect.IO, LoadImageErrors] = me
+
+  implicit override def executionContext = scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
+
+  LoadImageAction.execute(existingPath).value.unsafeToFuture().map(println)
+
+}
+
+
 class ActionTest extends AsyncFunSpec with AsyncIOSpec with Matchers{
 
   given  EnvironmentWithExecutionContext  = new EnvironmentWithExecutionContext {
@@ -31,16 +49,32 @@ class ActionTest extends AsyncFunSpec with AsyncIOSpec with Matchers{
 
   implicit override def executionContext = scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
 
+  val existingPath = "/images/stefan.png"
+  val nonExistingPath = "/images/stefans.png"
+
   describe("An ImageAction") {
-    describe("when accessing") {
+    describe("when checking") {
       describe("an non existing File") {
-        it("should return a daomain excption") {
-          LoadImageAction.checkFileAccess("/images/stefans.png").value.asserting( _.isLeft shouldBe true)
+        it("should return a domain excption") {
+          LoadImageAction.checkFileAccess(existingPath).value.asserting( _.isRight shouldBe true)
         }
       }
       describe("an existing File") {
         it("should return unit") {
-          LoadImageAction.checkFileAccess("/images/stefan.png").value.asserting( _.isRight shouldBe true)
+          LoadImageAction.checkFileAccess(nonExistingPath).value.asserting( _.isLeft shouldBe true)
+        }
+      }
+    }
+    describe("when opening") {
+      describe("an non existing File") {
+        it("should return a domain excption") {
+          LoadImageAction.execute(existingPath).value.unsafeToFuture().map(println)
+          true shouldBe true
+        }
+      }
+      describe("an existing File") {
+        it("should return unit") {
+          LoadImageAction.execute(nonExistingPath).value.asserting( _.isRight shouldBe true)
         }
       }
     }
