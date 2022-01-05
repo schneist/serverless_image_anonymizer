272ffae12992fddb459565ce91fb2b761a647cbe
root <test@test.com>
2022-01-05 13:25:21 +0000

    Changes by schneist

---
+++ nodefs/src/test/scala/nodefs/NodeFsTest.scala
@@ -0,0 +1,13 @@
+package nodefs
+
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
+
+class NodeFsTest  extends AnyFunSpec  with Matchers {
+
+  describe ("Loaded Bookings" ) {
+
+    it("should not load an unreachable file") {
+    }
+  }
+}
